37946e2f10fe74178761aa74a9ce001c2178285f
Lucas Mezêncio <lucas.mezencio@gmail.com>
2017-07-11 11:28:47 -0300

    Improve Movie documentation

--- media.py
+++ media.py
@@ -4,7 +4,18 @@ Media module
 
 
 class Movie(object):
-    """Holds infos about a Movie"""
+    """Holds infos about a Movie
+
+    Args:
+        title: Movie's title
+        storyline: Movie's storyline
+        poster_image_url: Movie's poster image URL
+        trailer_youtube_url: Movie's trailer Youtube URL
+    Behavior:
+        Create an instance of the Movie class
+    Returns:
+        void
+    """
 
     def __init__(self, title, storyline, poster_image_url,
                  trailer_youtube_url):
